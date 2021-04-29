08940ceab85d22d72dfb603bd28b940b186a0d78
IrleySilvestre <66978110+IrleySilvestre@users.noreply.github.com>
2021-04-29 20:39:49 -0300

    Versao inicial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
+
 class App extends Component {
-  render() {
-    return (
-      <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
-      </section>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            notas: []
+        }
+        this.addNotas = this.addNotas.bind(this);
+    }
+
+    addNotas(nota) {
+        const {txtTitulo, txtNota} = nota
+        const novaNota = {txtTitulo, txtNota}
+        const novoArrayNotas = [...this.state.notas,novaNota]
+        const novoEstado = {notas: novoArrayNotas}
+        this.setState(novoEstado)
+    }
+
+    render() {
+        return (
+            <section className="conteudo">
+                <FormularioCadastro addNotas = {this.addNotas}/>
+                <ListaDeNotas notas = {this.state.notas}/>
+            </section>
+        );
+    }
 }
 
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,25 +1,69 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./estilo.css";
+
 class FormularioCadastro extends Component {
-  render() {
-    return (
-      <form className="form-cadastro ">
-        <input
-          type="text"
-          placeholder="Título"
-          className="form-cadastro_input"
-        />
-        <textarea
-          rows={15}
-          placeholder="Escreva sua nota..."
-          className="form-cadastro_input"
-        />
-        <button className="form-cadastro_input form-cadastro_submit">
-          Criar Nota
-        </button>
-      </form>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.initialState = {
+            txtTitulo: '',
+            txtNota: ''
+        }
+
+        this.state = this.initialState
+
+        this.handleChangeTitulo = this.handleChangeTitulo.bind(this);
+        this.handleChangeNota = this.handleChangeNota.bind(this);
+        this.addNota = this.addNota.bind(this);
+    }
+
+    handleChangeTitulo(e) {
+        this.setState({
+            txtTitulo: e.target.value
+        })
+    }
+
+    handleChangeNota(e) {
+        e.stopPropagation()
+        this.setState({
+            txtNota: e.target.value
+        })
+    }
+
+    addNota(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        this.props.addNotas(this.state)
+        this.setState(this.initialState)
+    }
+
+    render() {
+        const {txtTitulo, txtNota} = this.state
+        return (
+            <form className="form-cadastro ">
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro_input"
+                    id="txtTitulo"
+                    name="txtTitulo"
+                    value={txtTitulo}
+                    onChange={this.handleChangeTitulo}
+                />
+                <textarea
+                    rows={15}
+                    placeholder="Escreva sua nota..."
+                    className="form-cadastro_input"
+                    id="txtNota"
+                    name="txtNota"
+                    value={txtNota}
+                    onChange={this.handleChangeNota}
+                />
+                <button className="form-cadastro_input form-cadastro_submit" onClick={this.addNota}>
+                    Criar Nota
+                </button>
+            </form>
+        );
+    }
 }
 
 export default FormularioCadastro;
